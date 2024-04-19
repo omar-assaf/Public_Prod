b70cc9b22f49afbdb184059be0970b296214bc25
Omar <61846615+omar-assaf@users.noreply.github.com>
2024-04-19 17:22:09 +0200

    Update detecting if SN is enrolled in Intune

--- Intune/Group_Add_Multiple_Objects.ps1
+++ Intune/Group_Add_Multiple_Objects.ps1
@@ -18,7 +18,7 @@
 # groups, using MSGraph powerShell.
 ####################################################
 #Connect MS GRAPH Powershell SDK
-Connect-MgGraph
+Connect-MgGraph -NoWelcome
 #import device Managment Graph module
 Import-Module Microsoft.Graph.DeviceManagement
 #import directory managment module
@@ -30,14 +30,30 @@ $GroupID = "0a90251a-bd9e-430d-86e5-88eb04c5d24b"
 $Groupdetails = (Get-MgGroup -GroupId $GroupId).DisplayName
 
 $DeviceList = Import-Csv "C:\Users\oas\Downloads\DeviceList.csv"
-ForEach ($SerialNumber in $DeviceList.SN) {
-	$DeviceIntuneIDlist = Get-MgDeviceManagementManagedDevice -Filter "serialNumber eq '$SerialNumber'" -Property "id" 
-	ForEach ($DeviceIntuneID in $DeviceIntuneIDlist.Id) {
-    		$EntraDeviceIDlist = Get-MgDevice -CountVariable CountVar -Search deviceId:$DeviceIntuneID -Property "Id"  -ConsistencyLevel eventual
-        	ForEach ($DeviceEntraID in $EntraDeviceIDlist.Id)
-				{
-        			New-MgGroupMember -GroupId $GroupsID -DirectoryObjectId $DeviceEntraID
-				}
-				Write-Host "Serial Number=$SerialNumber DeviceIntuneID=$DeviceIntuneID  DeviceEntraID=$DeviceEntraID added $Groupdetails" -ForegroundColor green
-	}
+ForEach ($SerialNumber in $DeviceList.SN)
+{
+$DeviceIntuneIDlist = Get-MgDeviceManagementManagedDevice -Filter "serialNumber eq '$SerialNumber'" | select Id 
+    if ($null -eq $DeviceIntuneIDlist) 
+    {
+        Write-Host "Serial Number= $SerialNumber is not enrolled in Intune" -ForegroundColor Yellow
+    }
+    Else
+    {
+        ForEach ($DeviceIntuneID in $DeviceIntuneIDlist.Id)
+        {
+                $EntraDeviceIDlist = Get-MgDevice -CountVariable CountVar -Search deviceId:$DeviceIntuneID -Property "Id"  -ConsistencyLevel eventual
+                    ForEach ($DeviceEntraID in $EntraDeviceIDlist.Id)
+                    {
+                        try
+                            {
+                             New-MgGroupMember -GroupId $GroupID -DirectoryObjectId $DeviceEntraID
+                             Write-Host "Serial Number= $SerialNumber DeviceIntuneID= $DeviceIntuneID  DeviceEntraID= $DeviceEntraID added $Groupdetails" -ForegroundColor green
+                            }
+                        Catch
+                            {
+                             Write-Error "Exception Message: $($_.Exception.Message)"  
+                            }
+                    }
+        }
+    }
 }
