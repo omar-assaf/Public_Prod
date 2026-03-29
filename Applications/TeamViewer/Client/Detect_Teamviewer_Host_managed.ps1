9bfcacd5a4de6870709e59adcab3df7441d154d9
Omar <61846615+omar-assaf@users.noreply.github.com>
2026-03-29 03:42:58 +0200

    Updated new tools

--- Applications/TeamViewer/Client/Detect_Teamviewer_Host_managed.ps1
+++ Applications/TeamViewer/Client/Detect_Teamviewer_Host_managed.ps1
@@ -10,23 +10,22 @@
 ####################################################
 #	X: omar_assaf
 #    Omar Assaf
-# You can distribute or re-use in any way, as long 
 # as you give credit to initial script author ;)
 ####################################################
 #Announce the path of OCB TV installation
-$Path = "HKLM:\SOFTWARE\WOW6432Node\TeamViewer"
+$Path = "HKLM:\SOFTWARE\TeamViewer"
 
 #Get the value of the key that identify MSFOCB auto Teamviewer assignment
-$Version = Get-ItemPropertyValue -Path $Path -Name "Version"
-$Assigned = Get-ItemPropertyValue -Path $Path -Name "Device_Auto_Assigned_To_Account"
+$Version = Get-ItemPropertyValue -Path $Path -Name "Version" -ErrorAction SilentlyContinue
+$Assigned = Get-ItemPropertyValue -Path $Path -Name "Device_Auto_Assigned_To_Account" -ErrorAction SilentlyContinue
 
 if(($Version -like "* HC") -and ($Assigned -eq "1"))
 {
-Write-Host "Client Installed"
+Write-Host "Teamviewer Host for enterprise is detected and assigned."
 exit 0
 }
 else
 {
-Write-Host "Non-Standard Teamviewer Client"
+Write-Host "Non-Standard Teamviewer Host installation detected."
 exit 1
 }
